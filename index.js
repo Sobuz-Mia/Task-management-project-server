316c4c7c63d1d6dc38d57ef856ba55ebd89f9dea
Sobuz <sobuz.info0298@gmail.com>
2023-12-24 20:08:10 +0600

    delete api created

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send("Task management server is running")
-})
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.sbw5eqf.mongodb.net/?retryWrites=true&w=majority`;
 
-app.listen(port,()=>{
-    console.log(`Task management server is running port ${port}`)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const taskCollection = client.db("taskManagementDB").collection("tasks");
+    app.get("/allTask", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/tasks", async (req, res) => {
+      const query = req.body;
+      const result = await taskCollection.insertOne(query);
+      res.send(result);
+    });
+    app.delete("/task", async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Task management server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Task management server is running port ${port}`);
+});
